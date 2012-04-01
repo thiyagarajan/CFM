a6fa89e44890a65c115fb6341507b549580444e9
mahesh <mahesh@weboniselab.com>
2012-04-01 08:16:04 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cfm::Application.config.secret_token = '8ad5f3b7605048f12b8d7d3c4b6fdb87d5b1ad4a10f54e1fd984f437d40b5a91dd3b9ea190c5fc7b20569e563ad6732a88ecf0cf8da20cb6da6ec838c2ed054a'
